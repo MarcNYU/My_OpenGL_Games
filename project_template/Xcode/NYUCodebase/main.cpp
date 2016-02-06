9e4510423898d7806e9476056c7f4e00fb7ab078
Marcus Williams <maw593@nyu.edu>
2016-02-06 17:50:51 -0500

    project update

--- project_template/Xcode/NYUCodebase/main.cpp
+++ project_template/Xcode/NYUCodebase/main.cpp
@@ -5,6 +5,9 @@
 #include <SDL_opengl.h>
 #include <SDL_image.h>
 
+#include "ShaderProgram.h"
+#include "Matrix.h"
+
 #ifdef _WINDOWS
 #define RESOURCE_FOLDER ""
 #else
@@ -13,12 +16,30 @@
 
 SDL_Window* displayWindow;
 
+GLuint LoadTexture(const char *image_path);
+
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
     displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
+    
+    glViewport(0, 0, 640, 360);
+    ShaderProgram program(RESOURCE_FOLDER"vertex.glsl", RESOURCE_FOLDER"fragment.glsl");
+    //ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
+    
+    //GLuint tileTexture = LoadTexture("tile.png");
+    
+    Matrix projectionMatrix;
+    Matrix modelMatrix;
+    Matrix viewMatrix;
+    
+    projectionMatrix.setOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
+    
+    glUseProgram(program.programID);
+    
+    
 #ifdef _WINDOWS
     glewInit();
 #endif
@@ -31,10 +52,46 @@ int main(int argc, char *argv[])
                 done = true;
             }
         }
+        
+        program.setModelMatrix(modelMatrix);
+        program.setProjectionMatrix(projectionMatrix);
+        program.setViewMatrix(viewMatrix);
+        
+        //glBindTexture(GL_TEXTURE_2D, tileTexture);
+        
+        float vertices[] = {-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
+        
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+        glEnableVertexAttribArray(program.positionAttribute);
+        
+        //float texCoords[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
+        
+        //glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+        //glEnableVertexAttribArray(program.texCoordAttribute);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        glDisableVertexAttribArray(program.positionAttribute);
+        //glDisableVertexAttribArray(program.texCoordAttribute);
+        
+        
         glClear(GL_COLOR_BUFFER_BIT);
         SDL_GL_SwapWindow(displayWindow);
     }
     
     SDL_Quit();
     return 0;
+}
+
+GLuint LoadTexture(const char *image_path) {
+    SDL_Surface *surface = IMG_Load(image_path);
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, surface->pixels);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    SDL_FreeSurface(surface);
+    return textureID;
 }
